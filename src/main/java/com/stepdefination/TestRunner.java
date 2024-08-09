0088f637e87d63d408cd9e2a36bf249889f5aafc
shitalmaniyar <shitalmaniyar@gmail.com>
2024-08-09 11:11:12 +0530

    kalpesh branch master

---
+++ src/main/java/com/stepdefination/TestRunner.java
@@ -0,0 +1,18 @@
+package com.stepdefination;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+//@RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin = {"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
+		
+		features = {"src/test/resources/com/Features/DonateAmount.feature"}, 
+		glue = {"com.stepdefination"} ,dryRun=true
+		
+)
+
+public class TestRunner extends AbstractTestNGCucumberTests {
+	// Logger log = LogManager.getLogger(this.getClass());
+
+}
